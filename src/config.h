b99e3deefa4214f65278d1ffdf645f018f60d5b1
Travis <>
2018-12-06 11:49:02 +0100

    * ego can drive along the path * add spline.h * TODO: Avoid collision

---
+++ src/config.h
@@ -0,0 +1,17 @@
+//
+//  config.h
+//  Path_Planning
+//
+//  Created by Travis on 2018/12/5.
+//
+
+#ifndef config_h
+#define config_h
+
+namespace config
+{
+    int lane = 1;
+    float ref_vel = 49.5; //mph
+}
+
+#endif /* config_h */
